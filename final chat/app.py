e130d3983c265e17231c9e257c7accd0140246ca
Ashish-Coforge2 <ashish.2@coforge.com>
2025-07-15 16:25:00 +0530

    Revert "added mcp-clients path through app.py"
    This reverts commit 0b40442ee2997c47ec335b02e2e811ea594e74ca.

--- final chat/app.py
+++ final chat/app.py
@@ -6,9 +6,6 @@ import sys
 import os
 import uuid
 sys.path.append(os.path.abspath("../src/utils"))
-client_path = os.path.abspath("../src/mcp-clients")
-if client_path not in sys.path:
-    sys.path.append(client_path)
 from stm_context_manager import store_messages , get_conversation
 
 chat = ui.Chat(id="agent_chat")
